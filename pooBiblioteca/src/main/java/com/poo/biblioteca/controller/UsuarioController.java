4637cb7fe623b755b4289221ec5fe33f72f324e7
Gabriel Pequeno <gabriel.pequeno@fornecedores.goias.gov.br>
2023-12-18 18:41:25 -0300

    Criação e padronização de todos os endpoints, mapemento de dto para entidade e de entidade para dto

--- pooBiblioteca/src/main/java/com/poo/biblioteca/controller/UsuarioController.java
+++ pooBiblioteca/src/main/java/com/poo/biblioteca/controller/UsuarioController.java
@@ -1,22 +1,67 @@
 package com.poo.biblioteca.controller;
 
+import com.poo.biblioteca.dto.UsuarioDto;
+import com.poo.biblioteca.mapper.UsuarioMapper;
 import com.poo.biblioteca.model.Usuario;
 import com.poo.biblioteca.service.UsuarioService;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.SortDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/usuario")
 public class UsuarioController {
+
     private final UsuarioService usuarioService;
-    @Autowired
+
     public UsuarioController(UsuarioService usuarioService) {
         this.usuarioService = usuarioService;
     }
 
+    @GetMapping(path = "/paginado", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public Page<UsuarioDto> buscarUsuarios(
+            UsuarioDto filter,
+            @PageableDefault
+            @SortDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<Usuario> pageUsuario = this.usuarioService.findAll(UsuarioMapper.INSTANCE.dtoParaEntidade(filter), pageable);
+        //use map para converter cada entidade para DTO
+        Page<UsuarioDto> pageUsuarioDto = pageUsuario.map(UsuarioMapper.INSTANCE::entidadeParaDto);
+
+        return pageUsuarioDto;
+    }
+
+    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public UsuarioDto buscarUsuarioPorId(@PathVariable Long id) {
+        var usuario = usuarioService.findById(id).orElseThrow(() -> new EntityNotFoundException("Usuario : " + id));
+        return UsuarioMapper.INSTANCE.entidadeParaDto(usuario);
+    }
 
     @PostMapping(path = "/salvarUsuario")
-    public Usuario salvarUsuario(@RequestBody Usuario usuario) {
-        return usuarioService.criarUsuario(usuario);
+    public UsuarioDto salvarUsuario(@RequestBody UsuarioDto usuarioDto) {
+        // Mapear o UsuarioDto para a entidade Usuario
+        Usuario usuario = UsuarioMapper.INSTANCE.dtoParaEntidade(usuarioDto);
+
+        // Chamar o serviço para criar o usuário com a entidade
+        Usuario usuarioCriado = usuarioService.criarUsuario(usuario);
+
+        // Pode retornar o mesmo objeto ou qualquer outra resposta desejada
+        // Aqui, estou retornando o UsuarioDto mapeado da entidade criada
+        return UsuarioMapper.INSTANCE.entidadeParaDto(usuarioCriado);
     }
+
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public UsuarioDto alterarUsuario(@RequestBody @Valid UsuarioDto usuarioDto) {
+        Usuario usuario = UsuarioMapper.INSTANCE.dtoParaEntidade(usuarioDto);
+        Usuario usuarioCriado = usuarioService.saveUpdate(usuario);
+        return UsuarioMapper.INSTANCE.entidadeParaDto(usuarioCriado);
+    }
+
 }
--- pooBiblioteca/src/main/java/com/poo/biblioteca/mapper/UsuarioMapper.java
+++ pooBiblioteca/src/main/java/com/poo/biblioteca/mapper/UsuarioMapper.java
@@ -1,6 +1,31 @@
 package com.poo.biblioteca.mapper;
 
-public interface UsuarioMapper {
+import com.poo.biblioteca.dto.UsuarioDto;
+import com.poo.biblioteca.model.Usuario;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
+@Mapper
+public interface UsuarioMapper {
+    UsuarioMapper INSTANCE = Mappers.getMapper(UsuarioMapper.class);
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "nome", target = "nome")
+    @Mapping(source = "endereco", target = "endereco")
+    @Mapping(source = "numeroContato", target = "numeroContato")
+    @Mapping(source = "email", target = "email")
+    @Mapping(source = "login", target = "login")
+    @Mapping(source = "senha", target = "senha")
+    Usuario dtoParaEntidade(UsuarioDto usuarioDto);
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "nome", target = "nome")
+    @Mapping(source = "endereco", target = "endereco")
+    @Mapping(source = "numeroContato", target = "numeroContato")
+    @Mapping(source = "email", target = "email")
+    @Mapping(source = "login", target = "login")
+    @Mapping(source = "senha", target = "senha")
+    UsuarioDto entidadeParaDto(Usuario usuario);
 
 }
