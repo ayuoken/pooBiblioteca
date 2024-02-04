19f752838c2409cd5d0228b286528616720d8e6a
ayuoken <luanm200406@gmail.com>
2024-02-03 21:51:01 -0300

    Implementação do Crud de livro

---
+++ pooBiblioteca/src/main/java/com/poo/biblioteca/dto/LivroDto.java
@@ -0,0 +1,21 @@
+package com.poo.biblioteca.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+public class LivroDto {
+
+    private Long id;
+
+    @NotNull(message = "Nome Obrigatório!")
+    private String nome;
+
+    @NotNull(message = "ISBN Obrigatório!")
+    private String isbn;
+
+}
---
+++ pooBiblioteca/src/main/java/com/poo/biblioteca/model/Livro.java
@@ -0,0 +1,25 @@
+package com.poo.biblioteca.model;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name="TB_LIVRO")
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "ID_LIVRO",nullable = false)
+    private Long id;
+
+    @Column(name = "NOME_LIVRO",nullable = false)
+    private String nomeLivro;
+
+    @Column(name = "ISBN_LIVRO", nullable = false)
+    private String isbn;
+
+}
