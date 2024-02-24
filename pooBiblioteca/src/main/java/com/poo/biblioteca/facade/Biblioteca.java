011cec87d2ff1b12bc54b620f387b8d25a6e44bf
Moonin371 <luanabrito285@gmail.com>
2024-02-23 23:35:20 -0300

    Notation das fachadas, agora elas funcionam

--- pooBiblioteca/src/main/java/com/poo/biblioteca/facade/Biblioteca.java
+++ pooBiblioteca/src/main/java/com/poo/biblioteca/facade/Biblioteca.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.poo.biblioteca.model.Livro;
@@ -14,6 +15,7 @@ import com.poo.biblioteca.repository.LivroRepository;
 
 import jakarta.persistence.EntityNotFoundException;
 
+@Service
 public class Biblioteca {
 	 @Autowired
 	    private LivroRepository livroRepository;
