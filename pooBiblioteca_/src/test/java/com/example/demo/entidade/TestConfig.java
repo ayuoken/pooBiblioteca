24b63bd355887d58b3cf059ede7490f34f27d0e9
Luan <117954536+ayuoken@users.noreply.github.com>
2024-03-01 15:58:54 -0300

    TestConfig.java

---
+++ pooBiblioteca_/src/test/java/com/example/demo/entidade/TestConfig.java
@@ -0,0 +1,11 @@
+package com.example.demo.entidade;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@SpringBootConfiguration
+public class TestConfig {
+}
